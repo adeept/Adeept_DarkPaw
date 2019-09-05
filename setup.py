03ecf49a0acc139a56ec1bba66a7356a90af3ad0
Adeept <support@adeept.com>
2019-09-05 17:50:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -117,7 +117,12 @@ for x in range(1,4):
 		break
 
 try:
-	os.system("sudo cd //home/pi/adeept_darkpaw/create_ap && sudo make install")
+	os.system("cd //home/pi/adeept_darkpaw/create_ap && sudo make install")
+except:
+	pass
+
+try:
+	os.system("cd //home/pi/create_ap && sudo make install")
 except:
 	pass
 
