62c3ab6f2721c46d7b3fe0a880bc9752b6e4577c
Devin <support@adeept.com>
2020-10-10 11:26:13 +0800

    update servo.py

--- server/servo.py
+++ server/servo.py
@@ -206,5 +206,10 @@ def get_direction():
 
 
 if __name__ == '__main__':
-	radar_scan()
-	pass
+	while True:
+		for i in range(0,100):
+			pwm.set_pwm(3, 0, (300+i))
+			time.sleep(0.05)
+		for i in range(0,100):
+			pwm.set_pwm(3, 0, (400-i))
+			time.sleep(0.05)
